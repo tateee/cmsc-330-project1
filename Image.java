928cf3ea191e942c5a3a8d874f5b434172743350
tateee <34150678+tateee@users.noreply.github.com>
2026-05-11 21:34:22 -0400

    Update Image.java

--- Image.java
+++ Image.java
@@ -1,7 +1,9 @@
-// CMSC 330 Advanced Programming Languages
-// Project 1 Skeleton
-// UMGC CITE
-// August 2021
+package cmsc330_project1;
+
+//Tate Prodigalidad
+//07-08-2025
+//CMSC 330 Advanced Programming Languages
+//Project 1
 
 import java.awt.*;
 
@@ -24,6 +26,9 @@ abstract class Image {
     }
 
     abstract void draw(Graphics graphics);
+
+	public void drawPolygon(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
 }
-
-
